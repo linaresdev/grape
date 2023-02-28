4d53ba56ce8174efb1c00d6928ba54b9cebab3fd
Ramon Linares <rlinareslf@gmail.com>
2023-02-28 10:07:02 -0400

    Iniciando segundo espacio de trabajo

---
+++ System/Core/Provider/CoreServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Projet\Providers\Path;
+/*
+*---------------------------------------------------------
+* ©IIPEC
+* Santo Domingo República Dominicana.
+*---------------------------------------------------------
+*/
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider {
+
+    public function boot() {}
+
+    public function register() {}
+}
---
+++ System/Driver.php
@@ -0,0 +1,41 @@
+<?php
+namespace Projet
+/*
+*---------------------------------------------------------
+* ©IIPEC
+*---------------------------------------------------------
+*/
+
+class Driver {
+
+    public function info() {
+
+        return [
+            'name'          => 'Grape',
+            'author'        => 'Ramon A Linares Febles',
+            'email'         => 'linareslf@gmail.com',
+            'license'       => 'Mit'
+            'support'       => 'https://iipec.net/application/grape',
+            'version'       => 'V-0.0',
+            'description'   => 'Es la uva para desarrollar grandes, medianos y pequeño proyectos.'
+        ];
+    }
+
+    public function app() {
+
+        return [
+            'type'      => 'package',
+            'slug'      => 'grape',
+            'driver'    => '\Grape\Driver::class',
+            'token'     => NULL,
+            'activated' => 1
+        ];
+    }
+
+    public function providers() { return []; }
+    public function alias() { return []; }
+
+    public function install($app) { }
+    public function destroy($app) { }
+
+}
